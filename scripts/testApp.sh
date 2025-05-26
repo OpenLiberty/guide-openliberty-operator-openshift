6a559088679b5f77131adbce4bceb048b75530af
Andrew Sasmito <Andrew.Sasmito@ibm.com>
2025-05-26 13:14:31 -0400

    Update to use maven wrapper
    Signed-off-by: Andrew Sasmito <Andrew.Sasmito@ibm.com>

--- scripts/testApp.sh
+++ scripts/testApp.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 set -euxo pipefail
-
-cd system
+./mvnw -version
 
 # Build the system/ app
-mvn -ntp -Dhttp.keepAlive=false \
+./mvnw -pl system \
+    -ntp -Dhttp.keepAlive=false \
     -Dmaven.wagon.http.pool=false \
     -Dmaven.wagon.httpconnectionManager.ttlSeconds=120 \
     -q clean package liberty:create liberty:install-feature liberty:deploy
 
 # Verifies that the system app is functional
-mvn -ntp liberty:start
+./mvnw -pl system \
+ -ntp liberty:start
 if curl "http://localhost:9080/health" | grep "UP" ; then exit $?; fi
 if curl "http://localhost:9080/system/properties" | grep "os.name" ; then exit $?; fi
 if curl -k "https://localhost:9443/health" | grep "UP" ; then exit $?; fi
 if curl -k "https://localhost:9443/system/properties" | grep "os.name" ; then exit $?; fi
-mvn -ntp liberty:stop
+./mvnw -pl system \
+    -ntp liberty:stop
 
 # Delete m2 cache after completion
 rm -rf ~/.m2
